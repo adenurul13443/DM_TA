574dda10f57dd575f8d1b2d80eb3bfc97af02aaa
adenurul13443 <91897439+adenurul13443@users.noreply.github.com>
2023-07-05 22:00:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import seaborn as sns
 
 st.title("Webb Klasterisasi Pengunjung Mall\nKlastering dengan K-Means\n")
 
-df = pd.read_csv('dataset-klastering.csv')
+df = pd.read_csv('data-mall.csv')
 
 df.rename(index=str, columns={
     'Annual Income (k$)' : 'Income',
@@ -40,10 +40,10 @@ ax.set_ylabel('inertia')
 
 st.header("Pencarian Elbow")
 #panah elbow
-ax.annotate('Possible elbow point', xy=(3, 140000), xytext=(3, 50000), xycoords='data',
+ax.annotate('Possible elbow point', xy=(3, 1900000), xytext=(3, 50000), xycoords='data',
             arrowprops=dict(arrowstyle='->', connectionstyle='arc3', color='blue', lw=2))
 
-ax.annotate('Possible elbow point', xy=(5, 80000), xytext=(2, 150000), xycoords='data',
+ax.annotate('Possible elbow point', xy=(5, 80000), xytext=(2, 2500000), xycoords='data',
             arrowprops=dict(arrowstyle='->', connectionstyle='arc3', color='blue', lw=2))
 
 #menampilkan panah elbow di streamlit
